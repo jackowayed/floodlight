41107baa7d96a5841f40ce5ae1ff46df34278ea3
behram mistree <bmistree@gmail.com>
2014-02-12 17:21:55 -0800

    Added further fields for flow table entries.

--- src/main/java/net/floodlightcontroller/pronghornmodule/PronghornFlowTableEntry.java
+++ src/main/java/net/floodlightcontroller/pronghornmodule/PronghornFlowTableEntry.java
@@ -3,11 +3,31 @@ package net.floodlightcontroller.pronghornmodule;
 
 public class PronghornFlowTableEntry
 {
+    public final static int DEFAULT_PRIORITY = 32767;
+    
     public String switch_id = null;
     public String entry_name = null;
     public boolean active = false;
     public String actions = null;
+    public Integer priority = DEFAULT_PRIORITY;
 
+    
+    public Integer ingress_port = null;
+    public String src_mac_address = null;
+    public String dst_mac_address = null;
+    public Integer vlan_id = null;
+    public Integer vlan_priority = null;
+    public Integer ether_type = null;
+    public Integer tos_bits = null;
+    public Integer protocol = null;
+
+    public String ip_src = null;
+    public String ip_dst = null;
+
+    public Integer src_port = null;
+    public Integer dst_port = null;
+
+    
     public PronghornFlowTableEntry(
         String switch_id,String entry_name,boolean active,
         String actions)
@@ -17,5 +37,4 @@ public class PronghornFlowTableEntry
         this.active = active;
         this.actions = actions;
     }
-            
 }
